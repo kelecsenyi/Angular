ed39b3dff1530f625539e45225d7967f3c3fa12c
Balázs <kelecsenyibalazs99@gmail.com>
2022-04-07 14:26:37 +0200

    ready

--- weatherapp/src/app/weather-main/weather-main.component.ts
+++ weatherapp/src/app/weather-main/weather-main.component.ts
@@ -17,18 +17,15 @@ export class WeatherMainComponent implements OnInit {
       isDay:true
     }
     this.getWeatherData();
-    console.log(this.WeatherData);
   }
 
   getWeatherData(){
-    fetch('{ "coord": {   "lon": -122.08,   "lat": 37.39 }, "weather": [   {     "id": 800,     "main": "Clear",     "description": "clear sky",     "icon": "01d"   } ], "base": "stations", "main": {   "temp": 282.55,   "feels_like": 281.86,   "temp_min": 280.37,   "temp_max": 284.26,   "pressure": 1023,   "humidity": 100 }, "visibility": 10000, "wind": {   "speed": 1.5,   "deg": 350 }, "clouds": {   "all": 1 }, "dt": 1560350645, "sys": {   "type": 1,   "id": 5122,   "message": 0.0139,   "country": "US",   "sunrise": 1560343627,   "sunset": 1560396563 }, "timezone": -25200, "id": 420006353, "name": "Mountain View", "cod": 200 }')
+    fetch('https://api.openweathermap.org/data/2.5/weather?q=budapest&appid=a4820b2b7fd65cfcc5e5a0e36e277191')
     .then((result) => {
-      result.json();
+      return result.json();
     }).then((data) => {
-      this.setWeatherData(data);
+      return this.setWeatherData(data);
     });
-    //let data = JSON.parse('{ "coord": {   "lon": -122.08,   "lat": 37.39 }, "weather": [   {     "id": 800,     "main": "Clear",     "description": "clear sky",     "icon": "01d"   } ], "base": "stations", "main": {   "temp": 282.55,   "feels_like": 281.86,   "temp_min": 280.37,   "temp_max": 284.26,   "pressure": 1023,   "humidity": 100 }, "visibility": 10000, "wind": {   "speed": 1.5,   "deg": 350 }, "clouds": {   "all": 1 }, "dt": 1560350645, "sys": {   "type": 1,   "id": 5122,   "message": 0.0139,   "country": "US",   "sunrise": 1560343627,   "sunset": 1560396563 }, "timezone": -25200, "id": 420006353, "name": "Mountain View", "cod": 200 }');
-    //this.setWeatherData(data);
   }
 
   setWeatherData(data:any){
